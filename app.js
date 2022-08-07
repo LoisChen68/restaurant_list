bc57845edffe6722d66f690c9882b5090ea1a872
LoisChen68 <a8283626@gmail.com>
2022-08-07 23:08:48 +0800

    feat: modify index.handlebars

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const port = 3000
 //載入handlebars
 const exphbs = require('express-handlebars')
 
+//載入餐廳.json
+const restaurantData = require('./restaurant.json').results
+
 //設定模板引擎
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 //設定首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurantData })
 })
 
 //設定監聽
