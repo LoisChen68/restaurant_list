ff713f2724d4b04d45aca3f918edb23dde87f190
LoisChen68 <a8283626@gmail.com>
2022-08-07 22:53:45 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//載入handlebars
+const exphbs = require('express-handlebars')
+
+//設定模板引擎
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 //設定首頁路由
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.render('index')
 })
-
-
 
 //設定監聽
 app.listen(port, () => {
